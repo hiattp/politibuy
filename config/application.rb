da83ea92d66262d13e26760dcabfd6047f59f88e
Paul Hiatt <hiattp@gmail.com>
2012-09-23 13:47:44 -0400

    index page up and running, devise installed

--- config/application.rb
+++ config/application.rb
@@ -55,6 +55,9 @@ module Politibuy
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    
+    # May need this for heroku deploy
+    config.assets.initialize_on_precompile = false
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
