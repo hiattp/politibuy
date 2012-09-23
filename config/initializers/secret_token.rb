e72a7b48d65798c8f715c6cd4923f9fdcc8230fd
Paul Hiatt <hiattp@gmail.com>
2012-09-23 13:22:09 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Politibuy::Application.config.secret_token = '17543fcfde0ae0d93607f2e1aa46128889a86719598dba30600706ee743eb5bd553e1d0ae1a9712a847529b40f0ea652e5682169801bea4970568a79709a3bb2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Politibuy::Application.config.session_store :cookie_store, key: '_politibuy_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Politibuy::Application.config.session_store :active_record_store
