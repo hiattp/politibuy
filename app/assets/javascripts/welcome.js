877367d6401570d7e94a8a089b6a5b540fc19b79
Paul Hiatt <hiattp@gmail.com>
2012-09-25 13:00:08 -0400

    added ruby type to gemfile

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,9 +1,2 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-
-$(document).ready(function(){
-  $("a").click(function(e){
-    e.preventDefault();
-    return false;
-  });
-});
