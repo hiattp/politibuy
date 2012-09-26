cdd0ceef3b7d7e5336d27175483c6f8ea663ddd9
Paul Hiatt <hiattp@gmail.com>
2012-09-26 14:32:16 -0400

    finished first design of campaign show page

--- app/assets/javascripts/campaigns.js
+++ app/assets/javascripts/campaigns.js
@@ -1,2 +1,15 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$(document).ready(function(){
+  $("li.participant-wrapper").hover(
+    function(e){
+      $(".participant-amount-wrapper", this).show();
+      $("h4.participant-amount", this).show();
+    },
+    function(e){
+      $(".participant-amount-wrapper", this).hide();
+      $("h4.participant-amount", this).hide();      
+    }
+  );
+});
