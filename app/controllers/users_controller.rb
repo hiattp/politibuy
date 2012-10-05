a51673d126e493c3dc875ee02995ac4f10af23e7
Paul Hiatt <hiattp@gmail.com>
2012-10-05 14:26:42 -0400

    adding user account settings pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,12 +14,12 @@ class UsersController < ApplicationController
   end
   
   def bulk_invite
-      authorize! :bulk_invite, @user, :message => 'Not authorized as an administrator.'
-      users = User.where(:confirmation_token => nil).order(:created_at).limit(params[:quantity])
-      users.each do |user|
-        user.send_confirmation_instructions
-      end
-      redirect_to :back, :only_path => true, :notice => "Sent invitation to #{users.count} users."
+    authorize! :bulk_invite, @user, :message => 'Not authorized as an administrator.'
+    users = User.where(:confirmation_token => nil).order(:created_at).limit(params[:quantity])
+    users.each do |user|
+      user.send_confirmation_instructions
     end
+    redirect_to :back, :only_path => true, :notice => "Sent invitation to #{users.count} users."
+  end
   
 end
